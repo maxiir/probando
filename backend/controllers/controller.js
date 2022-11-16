ac2f898adabc481e480569790794d061003181a1
Maxi <maxiirucci@gmail.com>
2022-11-16 01:09:53 -0300

    new change add

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -5,8 +5,22 @@ controller.createPeople = async (req,res) => {
     const {nombre,telefono,localidad} = req.body
     const savedata = new Modelo ({nombre:nombre,telefono:telefono,localidad:localidad})
     await savedata.save()
-    // const data = req.body
-    // console.log(nombre,telefono,localidad)
+    
 }
 
+controller.getPeople = async (req,res) =>{
+    
+    const {id} = req.params
+    const getdata = await Modelo.findById(id)
+    console.log(getdata)
+    
+}
+
+
 export default controller;
+
+
+// save() --> guardar datos en ddbb
+// find() o findById(el id)--> busca en ddbb por id
+// updateOne({_id: el id},{campos}) --> actualiza datos por id
+// remove({_id: el id}) --> eliminar datos de ddbb por id
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,6 +4,6 @@ import controller from '../controllers/controller.js'
 const router = Router()
 
 router.post('/api/newpeople', controller.createPeople)
-
+router.get('/api/getpeople/:id', controller.getPeople)
 
 export default router;
